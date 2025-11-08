9c4805e810a5c6b84b991c710fc4ae4514014d93
Frank1o3 <jahdy1o3@gmail.com>
2025-11-08 11:46:37 -0400

    little name change (it will not hurt anyone :D) 	renamed:    src/tracker.py -> src/aimbot.py 	modified:   src/main.py

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@
 import os
 
 from config import Config
-from tracker import Tracker
+from aimbot import Tracker
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
 config_path = os.path.join(os.path.dirname(script_dir), "config.yml")
